5ba1428d46e8dd02934c5575dd6fd7bca2b1fc8d
Eulerson314 <baptistegenest@gmail.com>
2024-02-28 19:46:48 +0100

    up site

---
+++ bib/bib.js
@@ -0,0 +1,10 @@
+var nesots_bib = `
+@article{NESOTS24,
+ authors = {Baptiste Genest, Nicolas Courty and David Coeurjolly},
+ journal = {Computer Graphics Forum (Proceedings of Eurographics)},
+ month = {April},
+ number = {2},
+ title = {Non-Euclidean Sliced Optimal Transport Sampling},
+ volume = {43},
+ year = {2024}
+}`;
